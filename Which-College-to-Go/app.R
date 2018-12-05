2576c3829a91a9b6af114319e70d53821c417823
丁村鸿鹄 <dingcunhonghu@dingcunhonghudeMacBook-Air.local>
2018-12-05 07:41:44 -0500

    popup k_median

--- Which-College-to-Go/app.R
+++ Which-College-to-Go/app.R
@@ -21,10 +21,10 @@ college <- read_rds("college.rds")
 
 states <- states(cb=T)
 
-# Deriving the options for the "state".
+# Deriving the options for the "college".
 
-state_choices <- college %>% 
-  group_by(state) %>% 
+college_choices <- college %>% 
+  group_by(name) %>% 
   summarise()
 
 # Define the UI
@@ -41,10 +41,10 @@ ui <- fluidPage(theme = shinytheme("slate"),
                     
                     # Select Input for "State".
                     
-                    selectInput(inputId = "state",
-                                label = "State",
-                                choices = state_choices,
-                                selected = "NY"),
+                    selectInput(inputId = "college",
+                                label = "Name of the College",
+                                choices = college_choices,
+                                selected = "Harvard University"),
                     
                     # And a button allowing users to download my data and further poke around 
                     
@@ -81,34 +81,13 @@ ui <- fluidPage(theme = shinytheme("slate"),
 
 server <- function(input, output) {
   
-  # Geojoining the states geolayer and the cleaned mappable data.
-  
-  data <- geo_join(states, college, "STUSPS", "state")
-  
-  # Creating a color palette based on the number range in the total column
-  
-  pal <- colorNumeric("BuPu", domain=data$mr_kq5_pq1)
-  
-  # popup the income of students
-  
-  popup_sb <- paste0(as.character(data$name), ": ", as.character(data$k_median))
-  
   # render map
   
   output$mymap <- renderLeaflet({
     leaflet() %>% 
     addProviderTiles(provider = "CartoDB") %>%
     setView(-98.483330, 38.712046, zoom = 4) %>%
-    addMarkers(lng = data$lon, lat =data$lat, popup = ~popup_sb) %>%
-    addPolygons(data = data , 
-                fillColor = ~pal(data$mr_kq5_pq1), 
-                fillOpacity = 0.7, 
-                weight = 0.2, 
-                smoothFactor = 0.2) %>%
-    addLegend(pal = pal, 
-              values = data$mr_kq5_pq1, 
-              position = "bottomright", 
-              title = "Mobility Rate: Percent of students who have parents in the Bottom 20% of the income distribution and reach the Top 20% of the income distribution") %>%
+    addMarkers(lng = college$lon, lat = college$lat, popup = as.character(college$k_median)) %>%
     addSearchOSM() %>%
     addReverseSearchOSM()
    })
