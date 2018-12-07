e989e8d4e3a2e66d0cd63487a27f1bd7f68ab900
丁村鸿鹄 <dingcunhonghu@dingcunhonghudeMacBook-Air.local>
2018-12-06 22:58:01 -0500

    change datatable colnames

--- Which-College-to-Go/app.R
+++ Which-College-to-Go/app.R
@@ -15,19 +15,36 @@ library(tidyverse)
 library(tigris)
 library(scales)
 
-# load data
+# load the rds data written previously
 
 college <- read_rds("college.rds")
 
-# Getting states geo files.
+# change the format of variables
 
-states <- states(cb=T)
+college <- college %>%
+  mutate(k_median = dollar(k_median)) %>%
+  mutate(par_median = dollar(par_median)) %>%
+  mutate(mr_ktop1_pq1 = percent(mr_ktop1_pq1/100)) %>%
+  mutate(par_q1 = percent(par_q1/100)) %>%
+  mutate(par_top1pc = percent(par_top1pc/100)) %>%
+  mutate(mr_kq5_pq1 = percent(mr_kq5_pq1/100)) %>%
+  mutate(trend_parq1 = percent(trend_parq1/100)) 
 
 # Deriving the options for the "college".
 
 college_choices <- college %>% 
   group_by(name) %>% 
   summarise()
+
+# Define variable choices for user
+
+variable_choices <- c("Child Income" = "k_median",
+                  "Parent Income" = "par_median",
+                  "Parents in the Bottom 20%" = "par_q1", 
+                  "Parents in the Top 1%" = "par_top1pc",
+                  "Mobility Rate" = "mr_kq5_pq1",
+                  "American Dream" = "mr_ktop1_pq1",
+                  "Change in % of Bottom 20% Parents" = "trend_parq1")
 
 # Define the UI
 # Use a pretty theme
@@ -41,17 +58,29 @@ ui <- fluidPage(theme = shinytheme("slate"),
                 sidebarLayout(
                   sidebarPanel(
                     
-                    # Select Input for "State".
+                    # Select Input for "college"
                     
                     selectInput(inputId = "college",
-                                label = "Name of the College",
+                                label = "Name of the College: ",
                                 choices = college_choices,
                                 selected = "Harvard University"),
+                    
+                    # Select Input for "Variable"
+                    
+                    selectInput(inputId = "variable",
+                                label = "Variable: ",
+                                choices = variable_choices,
+                                selected = "American Dream"),
                     
                     # And a button allowing users to download my data and further poke around 
                     
                     downloadButton(outputId = "download_data", 
-                                   label = "Download data")),
+                                   label = "Download data"),
+                    
+                    # variable explanations for people not familiar with the topic
+                    
+                    htmlOutput("description")),
+                    
                   
                   # Define the main panel
                   
@@ -64,8 +93,12 @@ ui <- fluidPage(theme = shinytheme("slate"),
                                 # Summary for tabs.
                                 
                                 tabPanel("About this map", 
-                                         HTML('<center><img src="thefadingame.jpg" height = 500 width = 550 ></center>'),
-                                         htmlOutput("about")),
+                                         htmlOutput("about"),
+                                         
+                                         # embed a picture
+                                         
+                                         HTML('<center><img src="thefadingame.jpg" height = 500 width = 550 ></center>')
+                                         ),
                                 
                                 # map output
                                 
@@ -82,13 +115,27 @@ ui <- fluidPage(theme = shinytheme("slate"),
 
 server <- function(input, output) {
   
+  # Variable Description 
+  
+  output$description <- renderText({
+    "<br/><b>Variable Description</b><br/>
+    <br/>1.<b>Child Income: </b><br/>Median child individual earnings in 2014
+    <br/>2.<b>Parents Income: </b><br/>Median parent household income
+    <br/>3.<b>Parents in the Bottom 20%: </b><br/>Fraction of parents in the Bottom 20% of the income distribution
+    <br/>4.<b>Parents in the Top 1%: </b><br/>Fraction of parents in the Top 1% of the income distribution
+    <br/>5.<b>Mobility Rate: </b><br/>Percent of students who have parents in the Bottom 20% of the income distribution and reach the Top 20% of the income distribution
+    <br/>6.<b>American Dream: </b><br/>(Upper-Tail Mobility Rate) Percent of students who have parents in the Bottom 20% of the income distribution and reach the Top 1% of the income distribution
+    <br/>7.<b>Change in % of Bottom 20% Parents: </b><br/>Change in % of Parents from the Bottom 20% of the income distribution between the 1980 and 1991 cohorts)
+"
+  })
+  
   # render readme
   
   output$about <- renderText({
     "<br/><b>Which college in the US offers you the best chance to achieve the American Dream?</b><br/><br/>
     This map is a map of opportunity. 
     It answers the question using data for over 30 million college students from 1999-2013.
-    The map offers you the insight of the median income and intergenerational mobility at each college in the United States. 
+    The map offers you the insight of the median income and intergenerational mobility at each college in the United States.<br/><br/> 
     "
   })
   
@@ -98,39 +145,40 @@ server <- function(input, output) {
     
     # filter data to input college
     
-    college <- college %>%
-      mutate(k_median = dollar(k_median)) %>%
-      mutate(mr_ktop1_pq1 = percent(mr_ktop1_pq1/100))
-    
     data <- college %>%
-      filter(name == input$college)
+      filter(name == input$college) %>%
+      select(name, lon, lat, input$variable)
     
     # setview of input college
     
     leaflet() %>% 
     addProviderTiles(provider = "CartoDB") %>%
     setView(data$lon, data$lat, zoom = 13) %>%
+    
+    # label markers as college name, and popup the variable user choose when clicking
+    
     addCircleMarkers(lng = college$lon, lat = college$lat, 
-                     popup = paste0("<b>", college$name, "</b>","<br/>", "Income Median: ",as.character(college$k_median),
-                                    "</b>","<br/>", "Mobility Rate: ", as.character(college$mr_ktop1_pq1)), 
-                     radius = 3) %>%
+                     label = college$name,
+                     popup = paste0(data[4]), 
+                     radius = 10) %>%
+    
+    # users can also search a certain address or region
+      
     addSearchOSM() %>%
     addReverseSearchOSM()
    })
   
-  # table of mobility
+  # table of variable users choose
   
   output$data <- renderDataTable({
-    
-    college <- college %>%
-      mutate(k_median = dollar(k_median)) %>%
-      mutate(mr_ktop1_pq1 = percent(mr_ktop1_pq1/100))
-    
     college %>%
-      arrange(desc(mr_ktop1_pq1)) %>%
-      select(name, state, mr_ktop1_pq1, par_median, k_median)
-
-    })
+      rename("Name of the College" = name,
+             "State" = state) %>%
+      
+      # I can't change the colname of input$variable
+      
+      select("Name of the College", State, input$variable)
+  })
     
   }
 
